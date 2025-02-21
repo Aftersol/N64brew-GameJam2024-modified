2fbb64269b3a8c897a81e893950dfe6ec4a46415
Aftersol <42478428+Aftersol@users.noreply.github.com>
2025-02-20 18:51:55 -0800

    add back example game with minor enchantments

---
+++ code/examplegame/examplegame.mk
@@ -0,0 +1,5 @@
+ASSETS_LIST += \
+	filesystem/examplegame/super_chicken.xm64 \
+	filesystem/examplegame/mash.wav64 \
+
+filesystem/examplegame/mash.wav64: AUDIOCONV_FLAGS += --wav-compress 1,3 --wav-mono --wav-resample 16000
